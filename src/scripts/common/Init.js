ad184faeb2a8973901620262360e838bb505a4bc
Vadim Veksler <35307996+reyzele@users.noreply.github.com>
2018-07-17 23:49:51 +0300

    change

--- src/scripts/common/Init.js
+++ src/scripts/common/Init.js
@@ -4,51 +4,53 @@ import { overlay } from '../main';
 
 function init() {
   const itemsList = document.querySelector('.basket__list');
-  const deleteItem = document.querySelector('#deleteItem');
+  const deleteItems = document.querySelector('#deleteItem');
 
   renderItems(itemsList, catalog);
   renderTotalPrice();
 
   const items = itemsList.querySelectorAll('.basket__item');
   let totalCount = 0;
-  let totalChecked = 0;
+  const checkedItems = [];
 
   setCount(totalCount);
 
   items.forEach((item, index) => {
     const checkbox = item.querySelector('.checkbox');
-    const countInput = item.querySelector('#count');
-
-    deleteItem.addEventListener('click', (e) => {
-      e.preventDefault();
-
-      if (checkbox.checked) {
-        dataChange(index, totalChecked);
-        checkbox.checked = false;
-        init();
-      }
-    })
+    const openCount = item.querySelector('#count');
 
     item.addEventListener('click', (e) => {
       if (e.target.classList.contains('checkbox-template')) {
-        const input = e.target.previousElementSibling;
+        const checkbox = e.target.previousElementSibling;
 
-        if (input.checked === false) {
+        if (!checkbox.checked) {
           totalCount += catalog[index].count;
-          totalChecked >= 0 && totalChecked++;
+          checkedItems.push(index)
         } else {
           totalCount -= catalog[index].count;
-          totalChecked >= 0 && totalChecked--;
+          checkedItems.splice(index, 1)
         }
         setCount(totalCount);
       }
     })
 
-    countInput.addEventListener('click', (e) => {
+    openCount.addEventListener('click', (e) => {
 
       overlay.open(e);
       overlay.setContent(e.target);
     })
+  })
+  
+  deleteItems.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (checkedItems.length > 0) {
+      checkedItems.forEach((item) => {
+        catalog.splice(item, 1);
+      })
+      
+      init();
+    }
   })
 }
 
